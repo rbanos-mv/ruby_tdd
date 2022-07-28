b28a0d9d14d0574d6e26196be26c06fd25fc0052
HectorTorresE <hectorjte@gmail.com>
2022-07-28 08:53:17 -0600

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,12 @@
+class NoNegativesException < StandardError
+end
+
 class Solver
-  
+  def factorial(number)
+    raise NoNegativesException, 'No negative numbers allowed' if number.negative?
+
+    return 1 if number.zero?
+
+    number * factorial(number - 1)
+  end
 end
